7c23a4308a83d71ee4dc769a3a94db0c97df748f
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-06-15 17:48:02 +0200

    Add settings pop up class

---
+++ LimitedSpace/Assets/Scripts/Menus/SettingsPopUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsPopUp : MonoBehaviour
+{
+    [SerializeField] private Button closeButton;
+    [SerializeField] private Button backgroundButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddListeners();
+    }
+
+    private void AddListeners()
+    {
+        closeButton.onClick.AddListener(Close);
+        backgroundButton.onClick.AddListener(Close);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
