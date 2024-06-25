0a65ba1cf681d30d44a3debb9ef8aea3253f1700
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-06-25 20:03:37 +0200

    Add custom canvas scaler to unify resolution across scenes

---
+++ LimitedSpace/Assets/Scripts/UI/CustomCanvasScaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class CustomCanvasScaler : CanvasScaler
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            uiScaleMode = ScaleMode.ScaleWithScreenSize;
+            referenceResolution = new Vector2(1080, 1920);
+            screenMatchMode = ScreenMatchMode.MatchWidthOrHeight;
+            matchWidthOrHeight = 0;
+            referencePixelsPerUnit = 100;
+        }
+    }
+}
