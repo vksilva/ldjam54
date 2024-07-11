780e2b2711855f715b4bf79916648214692558e6
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-07-11 22:49:23 +0200

    Update safearea

--- AMimir/Assets/Scripts/AppCore/SafeArea/SafeAreaRect.cs
+++ AMimir/Assets/Scripts/AppCore/SafeArea/SafeAreaRect.cs
@@ -1,7 +1,21 @@
-namespace AppCore
+using UnityEngine;
+
+namespace AppCore.SafeArea
 {
-    public class SafeAreaRect
+    [RequireComponent(typeof(RectTransform))]
+    public class SafeAreaRect : MonoBehaviour
     {
+        private RectTransform panel;
         
+        private void Awake()
+        {
+            panel = GetComponent<RectTransform>();
+        }
+
+        private void UpdatePanel(Vector2 anchorMin, Vector2 anchorMax)
+        {
+            panel.anchorMin = anchorMin;
+            panel.anchorMax = anchorMax;
+        }
     }
 }
--- AMimir/Assets/Scripts/AppCore/SafeArea/SafeAreaService.cs
+++ AMimir/Assets/Scripts/AppCore/SafeArea/SafeAreaService.cs
@@ -1,7 +1,47 @@
-namespace AppCore
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace AppCore.SafeArea
 {
-    public class SafeAreaService
+    public class SafeAreaService : MonoBehaviour
     {
+        [SerializeField] private bool useDebugSafeArea;
+        [SerializeField] private Rect debugSafeArea;
+
+        private readonly UnityEvent<Vector2, Vector2> updateSafeAreaEvent = new ();
+        private Rect SafeAreaRect => useDebugSafeArea ? debugSafeArea : Screen.safeArea;
+
+        private Vector2 anchorMin;
+        private Vector2 anchorMax;
+        private Rect previousSafeArea;
+
+        public void RegisterSafeArea(UnityAction<Vector2, Vector2> onUpdate)
+        {
+            updateSafeAreaEvent.AddListener(onUpdate);
+        }
         
+        private void Update()
+        {
+            UpdateSafeArea();
+        }
+
+        private void UpdateSafeArea()
+        {
+            if (SafeAreaRect == previousSafeArea)
+            {
+                return;
+            }
+
+            previousSafeArea = SafeAreaRect;
+            
+            anchorMin = SafeAreaRect.position;
+            anchorMax = SafeAreaRect.position + SafeAreaRect.size;
+            
+            // Normalize from 0 to 1
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+        }
     }
 }
