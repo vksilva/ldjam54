be755bed9b52a7dff58957648e37229d6f047ed5
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-07-15 17:23:03 +0200

    Add level name constant to utils

---
+++ AMimir/Assets/Scripts/Menus/LevelUtils.cs
@@ -0,0 +1,10 @@
+namespace Menus
+{
+    public class LevelUtils
+    {
+        public static string GetLevelName(int world, int level)
+        {
+            return $"world_{world:D2}_level_{level:D2}";
+        }
+    }
+}
