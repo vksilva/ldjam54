2c8f606f0a709ba4f391f7a2738236166e69db83
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-07-20 11:50:31 +0200

    Add Editor tool to add levels to Build Settings Remove level 00_00

--- AMimir/Assets/Scripts/Editor/EditorTools.cs
+++ AMimir/Assets/Scripts/Editor/EditorTools.cs
@@ -1,14 +1,40 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 
 namespace Busta.Editor
 {
     public static class EditorTools
     {
+        private static readonly string levelsPath = Path.Join("Assets", "Scenes", "Levels");
+        
         [MenuItem("Vanessa/Clean Player Prefs")]
         public static void CleanPlayerPrefs()
         {
             PlayerPrefs.DeleteAll();
+        }
+
+        [MenuItem("Vanessa/Add levels to Build Settings")]
+        public static void AddLevelsToBuildSettings()
+        {
+            var scenes = new List<EditorBuildSettingsScene>();
+            scenes.AddRange(EditorBuildSettings.scenes);
+
+            //Find levels
+            var worldDirectories = Directory.GetDirectories(levelsPath);
+            foreach (var dir in worldDirectories)
+            {
+                var files = Directory.GetFiles(dir, "*.unity");
+                foreach (var file in files)
+                {
+                    scenes.Add(new EditorBuildSettingsScene(file, true));
+                }
+            }
+
+            EditorBuildSettings.scenes = scenes.DistinctBy(s=> s.guid).ToArray();
         }
     }
 }
