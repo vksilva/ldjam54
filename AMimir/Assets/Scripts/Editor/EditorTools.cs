fa2666d2a14ce34e16467961e6b23ffb35eabf2f
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-07-22 22:15:57 +0200

    Create tool to fix cat colliders

--- AMimir/Assets/Scripts/Editor/EditorTools.cs
+++ AMimir/Assets/Scripts/Editor/EditorTools.cs
@@ -10,7 +10,9 @@ namespace Busta.Editor
     public static class EditorTools
     {
         private static readonly string levelsPath = Path.Join("Assets", "Scenes", "Levels");
-        
+        private static readonly string catsPath = Path.Join("Assets", "Prefabs", "Cats");
+        private static readonly string obstaclesPath = Path.Join("Assets", "Prefabs", "Obstacles");
+
         [MenuItem("Vanessa/Clean Player Prefs")]
         public static void CleanPlayerPrefs()
         {
@@ -34,7 +36,33 @@ namespace Busta.Editor
                 }
             }
 
-            EditorBuildSettings.scenes = scenes.DistinctBy(s=> s.guid).ToArray();
+            EditorBuildSettings.scenes = scenes.DistinctBy(s => s.guid).ToArray();
+        }
+
+        [MenuItem("Vanessa/Fix Cat Collider")]
+        public static void FixCatCollider()
+        {
+            Debug.Log($"Fix cat collider {catsPath}");
+            var files = Directory.GetFiles(catsPath);
+            foreach (var file in files)
+            {
+                var cat = AssetDatabase.LoadAssetAtPath<PolygonCollider2D>(file);
+                if (!cat)
+                {
+                    continue;
+                }
+
+                Debug.Log($"Fixing {cat.name}");
+                for (var i = 0; i < cat.pathCount; i++)
+                {
+                    var path = cat.GetPath(i);
+                    path = path.Select(v => new Vector2(Mathf.Round(v.x), Mathf.Round(v.y))).ToArray();
+                    cat.SetPath(i, path);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
     }
 }
