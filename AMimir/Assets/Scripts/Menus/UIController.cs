6427a010b9fdd85a062473df72b0c20e3b3d7a89
Vanessa Oliveira <vksilva.oliveira@gmail.com>
2024-08-12 14:29:21 +0200

    Add cooldown to hint

--- AMimir/Assets/Scripts/Menus/UIController.cs
+++ AMimir/Assets/Scripts/Menus/UIController.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Globalization;
 using Busta.AppCore.Audio;
 using Busta.Gameplay;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Application = Busta.AppCore.Application;
@@ -12,14 +15,28 @@ namespace Busta.Menus
         [SerializeField] private Button pauseButton;
         [SerializeField] private PausePopUpController pausePopUp;
         [SerializeField] private Button hintButton;
+        [SerializeField] private Image hintButtonCooldownImage;
 
         private static AudioService _audioService;
+        
+        private static readonly float hintCooldown = 60f;
+        private float currentHintCooldown = 0;
 
         private void Start()
         {
             uiCanvas.gameObject.SetActive(true);
             GetServices();
             AddListeners();
+        }
+
+        private void Update()
+        {
+            if (currentHintCooldown > 0)
+            {
+                currentHintCooldown -= Time.deltaTime;
+                currentHintCooldown = Mathf.Max(0, currentHintCooldown);
+                hintButtonCooldownImage.fillAmount = currentHintCooldown / hintCooldown;
+            }
         }
 
         private static void GetServices()
@@ -35,7 +52,15 @@ namespace Busta.Menus
 
         private void OnHintButtonClicked()
         {
+            if (currentHintCooldown > 0)
+            {
+                Debug.Log($"Sem hint irmão.");
+                Debug.Log($"Tempo para proxima hint: {currentHintCooldown}");
+                return;
+            }
             GameController.Instance.ShowNewHint();
+            currentHintCooldown = hintCooldown;
+            hintButtonCooldownImage.fillAmount = 1f;
         }
 
         private void OnPauseButtonClicked()
